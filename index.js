ac660b07b944c83e9968cc54a65160799c16b654
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-05-20 21:35:02 +0200

    using querystring parameter

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 exports.handler = (event, context, callback) => {
-    callback(null, 'Hello World')
+  const name = event.queryStringParameters && event.queryStringParameters.name
+    ? event.queryStringParameters.name
+    : 'World'
+
+  const body = JSON.stringify({
+    message: `Hello ${name}`
+  })
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body
+  }
+
+  return callback(null, response)
 }
